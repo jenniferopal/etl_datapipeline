46ab54f906dc561570080a3bf5407ea23bc03c10
Jennifer Opal <contact@jenniferopal.co>
2025-01-16 22:22:43 +0000

    visualisation script tbc

---
+++ scripts/visualise.py
@@ -0,0 +1,6 @@
+import pandas as pd # type: ignore
+import seaborn as sns # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
+
+def visualise(data):
+    #create a data visualisation for analysis. sour
